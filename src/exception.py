78859f8788e332f06aef347d55a764eaf4c195f5
AakashEngineer <aakashpal1183@gmail.com>
2024-08-25 15:22:35 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+def error_message_details(error, error_details:sys):
+    _, _, tb = sys.exc_info()
+    file_name  = tb.tb_frame.f_code.co_filename
+    error_message = 'error in file: ' + file_name + 'Line no: ' + str(tb.tb_lineno) + 'Error: ' + str(error)
+    return error_message
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details)
+
+    def __str__(self) -> str:
+        return self.error_message
+
+
